9eeb2ad9971a51503ab9c45ff9a5d5baf8a496e9
Raj Keshari <rajkeshari@gmail.com>
2024-08-01 17:00:08 +0530

    add public file to 1 basicreact

---
+++ 1basicreact/src/App.js
@@ -0,0 +1,13 @@
+
+import Chai from './Chai.js'
+
+function App() {
+  return (
+    <>
+    <Chai/>
+    <h1>chai ar react</h1>
+    </>
+   );
+}
+
+export default App
---
+++ 1basicreact/src/Chai.js
@@ -0,0 +1,9 @@
+//import Chai from './Chai.js'
+
+function Chai(){
+    return(
+        <h1>this is bautiful world And Raj is part of this marvolous World</h1>
+    )
+}
+
+export default Chai
